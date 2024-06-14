7a7ded0d8446623ec3b56a0bc8f91f4e0e14d18b
Brady Green <138686566+Brady-G@users.noreply.github.com>
2024-06-14 13:21:45 -0230

    Switch to maven

--- cli/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    java
-}
-
-group = "brady.green"
-version = "0.0.1"
-
-java.toolchain.languageVersion = JavaLanguageVersion.of(17)
--- settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-rootProject.name = "sprint1"
-
-include("cli")
